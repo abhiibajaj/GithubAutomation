ea234af81b86140297dde2dd19ec51636713a370
Arpit Bajaj <arpitbajaj98@gmail.com>
2019-11-23 19:27:21 +1100

    Added files working

--- create.py
+++ create.py
@@ -10,7 +10,7 @@ def main():
     access_token = get_access_token()
     repo_name = sys.argv[1]
     githubBot = GithubBot(access_token)
-    githubBot.create()
+    githubBot.create(repo_name)
 
 
 def get_access_token():
@@ -18,6 +18,7 @@ def get_access_token():
     config_parser = configparser.ConfigParser()
     config_parser.read(config)
     access_token = config_parser['AUTH']['token']
+    return access_token
 
 
 if __name__ == '__main__':
--- create.sh
+++ create.sh
@@ -5,35 +5,36 @@ function create() {
     currDir=($PWD)
     dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
     cd "$dir"
+    
     if [ "$1" == "" ]; then
         echo "You're missing a project name!";
     else
-        # python3 create.py $1
+        python create.py $1
         cd "$projectPath"
         mkdir "$1"
         cd "$1"
         
         file="main.py"
         if [  "$2" != "" ]; then
-            file="main.${$2}"
+            file="main.$2"
         fi
         
         git init
-        touch file
+        git remote add origin git@github.com:arpitbajaj98/$1.git
+        touch $file
         touch "README.md"
         git add .
         git commit -m "Initial commit"
-        git push or
+        git push origin master
         
-        
-        # cd "$currDir"
+        cd "$currDir"
     fi
     
     
     
 }
 
-function test() {
+function remove() {
     h=($1)
     echo "$h"
 }
